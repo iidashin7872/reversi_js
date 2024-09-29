dfebf2402f4777735e90e9a5a97c1dce2d2baf4a
iidashin7872 <siniida12315@gmail.com>
2024-09-30 07:11:15 +0900

    turn change upate

--- reversi.js
+++ reversi.js
@@ -1,12 +1,26 @@
+let isOddTurn = true;
+
 $(function () {
     $(".square").click(clickSquareEvent);
 });
 
 function clickSquareEvent() {
     let square = $(this);
-    putPiece(square, "black")
+    putPiece(square, getTurnString())
+    changeTurn()
 }
 
 function putPiece(targetSquare, owner) {
     targetSquare.text("●").attr("data-owner", owner).addclass("selected");
+} 
+
+function getTurnString() {
+    if (isOddTurn) {
+        return "black";
+    }
+    return "white";
+}
+
+function changeTurn() {
+    isOddTurn = !isOddTurn;
 }
